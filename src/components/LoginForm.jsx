e2a7e33c83f5b212d93f905131987ba9311be5e3
joonsun <joonsun1006@naver.com>
2024-01-08 15:12:13 +0900

    feat: LoginForm 틀 작업

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,37 @@
+
+
 const LoginForm = () => {
   return (
-    <p>This is LoginForm</p>
+    <>
+      <h2>안녕하세요</h2>
+      <h2>~~~입니다.</h2>
+      <p>~~~~를 하는 서비스입니다.</p>
+
+      <form action="#">
+        <div>
+          <label htmlFor="loginEmail"></label>
+          <input type="email" id="loginEmail" placeholder="이메일 입력" />
+        </div>
+        <div>
+          <label htmlFor="loginPw"></label>
+          <input type="password" id="loginPw" placeholder="비밀번호 입력" />
+        </div>
+
+        <button>로그인</button>
+      </form>
+
+      <div>
+        <a href="#">이메일 찾기</a> | 
+        <a href="#">비밀번호 찾기</a> |
+        <a href="#">회원가입</a>
+      </div>
+      <p>SNS 계정으로 로그인</p>
+
+      <div>
+        a, b, c
+      </div>
+    </>
+
   )
 }
 
