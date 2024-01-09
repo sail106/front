7ffef5abd6f3a0e9325369f2f348ca84aefc0dd5
joonsun <joonsun1006@naver.com>
2024-01-09 12:03:56 +0900

    feat: #1 LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -34,49 +34,75 @@ const Button = styled.button`
 `;
 
 const AccountContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  text-align: center;
 `;
 
 const ATag = styled.a`
   text-decoration: none;
   color: black;
+  margin-left: 15px;
+  margin-right: 15px;
+  font-weight: bold;
+  color: #5d5253;
+`;
+
+const SnsWord = styled.p`
+  text-align: center;
+  color: #5d5253;
+  padding-top: 5px;
+  padding-left: 40px;
+  padding-bottom: 3px;
+`;
+
+const ImgContainer = styled.div`
+  text-align: center;
+  padding-bottom: 40px;
+`;
+
+
+const LogoImg = styled.img`
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  margin-left: 40px;
 `;
 
 const LoginForm = () => {
   return (
-    <>
-      <OuterDiv>
-        <h2>안녕하세요 :)</h2>
-        <h2>~~~입니다.</h2>
-        <p>_____ 를 하는 서비스입니다.</p>
+    <OuterDiv>
+      <h2>안녕하세요 :)</h2>
+      <h2>~~~입니다.</h2>
+      <p>_____ 를 하는 서비스입니다.</p>
 
-        <form action="#">
-          <div>
-            <label htmlFor="loginEmail"></label>
-            <Input type="email" id="loginEmail" placeholder="이메일 입력" />
-          </div>
-          <div>
-            <label htmlFor="loginPw"></label>
-            <Input type="password" id="loginPw" placeholder="비밀번호 입력" />
-          </div>
-
-          <Button>로그인</Button>
-        </form>
-
-        <AccountContainer>
-          <ATag href="#">이메일 찾기</ATag> | 
-          <ATag href="#">비밀번호 찾기</ATag> |
-          <ATag href="#">회원가입</ATag>
-        </AccountContainer>
-        <p>SNS 계정으로 로그인</p>
-
+      <form action="#">
         <div>
-          a, b, c
+          <label htmlFor="loginEmail"></label>
+          <Input type="email" id="loginEmail" placeholder="이메일 입력" />
         </div>
-      </OuterDiv>
+        <div>
+          <label htmlFor="loginPw"></label>
+          <Input type="password" id="loginPw" placeholder="비밀번호 입력" />
+        </div>
 
-    </>
+        <Button>로그인</Button>
+      </form>
+
+      <AccountContainer>
+        <ATag href="#">비밀번호 찾기</ATag>
+        <ATag href="#">회원가입</ATag>
+      </AccountContainer>
+
+      <SnsWord>
+        <p>SNS 계정으로 로그인</p>
+      </SnsWord>
+      <ImgContainer>
+        <a href="#"><LogoImg src="src/assets/naverFavicon.jpg" alt="네이버 로고" /></a>
+        <a href="#"><LogoImg src="src/assets/kakaoFavicon.png" alt="카카오 로고" /></a>
+        <a href="#"><LogoImg src="src/assets/googleFavicon.jpg" alt="구글 로고" /></a>
+      </ImgContainer>
+    </OuterDiv>
 
   )
 }
