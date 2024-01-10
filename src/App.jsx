4606251122a65e5fb7a5ab1ac9b0dbf9b6250fa7
joonsun <joonsun1006@naver.com>
2024-01-10 13:55:40 +0900

    refactor: #1 LoginForm component 분리

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import LoginForm from './components/LoginForm'
 import PasswordFindForm from './components/PasswordFindForm';
 import SpinnerTest from './components/SpinnerTest';
 import SignupForm from "./components/SignupForm";
+import Card from "./components/store/Card";
 
 function App() {
 
   return (
-    <>
+    <Card>
       <Routes>
         <Route path='/' element={<LoginForm />} />
         <Route path='/pw' element={<PasswordFindForm />} />
         <Route path='/test' element={<SpinnerTest />} />
       </Routes>
       <SignupForm />
-    </>
+    </Card>
   );
 };
 
