0c4b37d1ae3bbe0cd50f286df52e1a7c4aa8e29e
joonsun <joonsun1006@naver.com>
2024-01-10 10:35:46 +0900

    chore: #1 SignupForm start

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import {  Route, Routes } from "react-router-dom";
+
 import './App.css'
 import LoginForm from './components/LoginForm'
 import PasswordFindForm from './components/PasswordFindForm';
 import SpinnerTest from './components/SpinnerTest';
-import {  Route, Routes } from "react-router-dom";
+import SignupForm from "./components/SignupForm";
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<LoginForm />} />
-      <Route path='/pw' element={<PasswordFindForm />} />
-      <Route path='/test' element={<SpinnerTest />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/pw' element={<PasswordFindForm />} />
+        <Route path='/test' element={<SpinnerTest />} />
+      </Routes>
+      <SignupForm />
+    </>
   );
 };
 
