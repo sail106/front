b243b3529b2a28aec2470d3d66d0b07b58923e2f
joonsun <joonsun1006@naver.com>
2024-01-11 15:46:21 +0900

    style: #1 LoginHeader_add_font

--- src/components/login/LoginHeader.jsx
+++ src/components/login/LoginHeader.jsx
@@ -1,10 +1,19 @@
+import styled from "styled-components";
+
+const LoginBox = styled.h1`
+  text-align: center;
+  padding-bottom: 80px;
+
+  font-family: "Lexend Deca", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -1.2px;
+`;
+
 const LoginHeader = () => {
   return (
-    <>
-      <h2>안녕하세요 :)</h2>
-      <h2>~~~입니다.</h2>
-      <p>_____ 를 하는 서비스입니다.</p>
-    </>
+    <LoginBox>Flowering</LoginBox>
   )
 };
 
